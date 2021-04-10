6f2b30b21873b6a06647d13b233ad3ba754b4753
Myat Thu Ko <nymyatthuko@gmail.com>
2021-04-10 16:05:04 -0400

    Initial Commit

---
+++ Soul/Soul/SoulApp.swift
@@ -0,0 +1,17 @@
+//
+//  SoulApp.swift
+//  Soul
+//
+//  Created by Myat Thu Ko on 4/10/21.
+//
+
+import SwiftUI
+
+@main
+struct SoulApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
