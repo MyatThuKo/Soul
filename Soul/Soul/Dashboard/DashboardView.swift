d79c6a3f996d02598408a846869e876080f3863a
Aswani <aswanig@Aswanis-MacBook-Pro-2.local>
2021-04-10 15:34:58 -0700

    Dashboard and Firebase
    Included firebase auth functionalities and dashboard screen

---
+++ Soul/Soul/Dashboard/DashboardView.swift
@@ -0,0 +1,48 @@
+//
+//  DashboardView.swift
+//  Soul
+//
+//  Created by Aswani G on 4/10/21.
+//
+
+import SwiftUI
+
+struct DashboardView: View {
+    var body: some View {
+        TabView {
+            NavigationView {
+                ChartView()
+            }
+            .tabItem {
+                Image(systemName: "chart.bar.fill")
+                Text("Quiz")
+                    .font(.system(size: 12, weight: .light, design: .rounded))
+            }
+            NavigationView {
+                JournelingView()
+            }
+            .tabItem {
+                Image(systemName: "person.fill.questionmark")
+                Text("Quiz")
+                    .font(.system(size: 12, weight: .light, design: .rounded))
+            }
+            NavigationView {
+                SettingsView()
+            }
+            .tabItem {
+                Image(systemName: "gearshape")
+                Text("Settings")
+                    .font(.system(size: 12, weight: .light, design: .rounded))
+            }
+
+        }
+    
+    }
+}
+
+struct DashboardView_Previews: PreviewProvider {
+    static var previews: some View {
+        DashboardView()
+    }
+}
+
