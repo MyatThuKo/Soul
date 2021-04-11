277b8d5fdad7aacc78390bc8b229433b23b074cb
markkypanc <rphanchotong@gmail.com>
2021-04-10 17:21:47 -0700

    Implement sign-in and register view

---
+++ Soul/Soul/View/RegisterView.swift
@@ -0,0 +1,89 @@
+//
+//  RegisterView.swift
+//  Soul
+//
+//  Created by Mark Rachapoom on 10/4/21.
+//
+
+import SwiftUI
+
+struct RegisterView: View {
+    // MARK: - PROPERTIES
+    
+    @Environment(\.presentationMode) var presentation
+    
+    @State private var email: String = ""
+    @State private var password: String = ""
+    
+    // MARK: - BODY
+    var body: some View {
+            ZStack(alignment: .leading) {
+                Color("background-color")
+                    .ignoresSafeArea(.all)
+                VStack {
+                    // MARK: - HEAD
+                    HStack {
+                        Button(action: {
+                            self.presentation.wrappedValue.dismiss()
+                        }, label: {
+                            Image(systemName: "chevron.left")
+                                .font(.title)
+                                .foregroundColor(Color("foreground-color"))
+                        })
+                        Spacer()
+                    }//: HSTACK
+                    .padding(.vertical, 10)
+                    .padding(.horizontal, 20)
+                    
+                    VStack(alignment: .leading, spacing: 12) {
+                        Text("Register")
+                            .font(.system(size: 44, weight: .bold, design: .rounded))
+                        Text("Sign up to get started")
+                            .font(.system(size: 30, weight: .semibold, design: .rounded))
+                    }//: VSTACK
+                    .padding(.vertical, 20)
+                    .padding(.horizontal, 45)
+                    .offset(x: -25)
+                    
+                    Spacer()
+                    
+                    // MARK: - TEXT FIELD
+                    TextFieldView(email: $email, password: $password)
+                        .padding()
+                    
+                    Spacer()
+                    
+                    // MARK: - SIGNIN AND REGISTER
+                    VStack(spacing: 0) {
+                        
+                        HStack {
+                            Text("hidden text")
+                                .font(.system(size: 17,weight: .bold, design: .rounded))
+                        }//: HSTACK
+                        .foregroundColor(Color("background-color"))
+                        
+                        Button(action: {
+                            // SIGN UP ACTION
+                        }, label: {
+                            Text("Create Account")
+                                .font(.system(size: 19, weight: .bold, design: .rounded))
+                        })//: BUTTON
+                        .padding(.vertical, 40)
+                        .background(
+                            Color.primary
+                                .frame(width: 345, height: 64, alignment: .center)
+                                .cornerRadius(17)
+                        )
+                        .foregroundColor(Color("background-color"))
+                        .navigationBarHidden(true)
+                    }//: VSTACK
+                }//: VSTACK
+            }//: ZSTACK
+    }
+}
+
+struct RegisterView_Previews: PreviewProvider {
+    static var previews: some View {
+        RegisterView()
+    }
+}
